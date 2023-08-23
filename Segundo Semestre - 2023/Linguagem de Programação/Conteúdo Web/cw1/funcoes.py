702f3ebcae2accfa0a840bcc7048f25da2f60248
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-08-23 15:08:40 -0300

    Funções em python

---
+++ Segundo Semestre - 2023/Linguagem de Programação/Conteúdo Web/cw1/funcoes.py
@@ -0,0 +1,5 @@
+def imprimir_mensagem(diciplina, curso):
+    print(f"Minha primeira função em Python desenvolvida na diciplina: {diciplina}, do curso: {curso}.")
+
+imprimir_mensagem("Python", "ADS")
+imprimir_mensagem("Java", "ADS")
