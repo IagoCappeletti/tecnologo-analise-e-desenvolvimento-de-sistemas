f07abf64d5859dfbdb7019a8a225cbeba738fa02
Iago Cappeletti <iago_cappeletti@hotmail.com>
2023-08-23 11:51:13 -0300

    Usando list em python

---
+++ Segundo Semestre - 2023/Linguagem de Programação/Conteúdo Web/cw1/SequenciaNumerica.py
@@ -0,0 +1,5 @@
+print(list(range(10))) # Gera uma sequência de 0 a 9.
+print(list(range(1, 12))) # Gera uma sequência de 1 a 11.
+print(list(range(0, 30, 5))) # Gera uma sequência de 0 a 30 com passo de 5.
+print(list(range(0, -10, -1))) # Gera uma sequência numérica negativa.
+print(list(range(0))) # Gera uma lista vazia.
